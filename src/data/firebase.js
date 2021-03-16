50d694741e327c8ebfe2d87ec164fa6093c554d7
Jean-Luc Ho <jeanluc.ho@hotmail.com>
2021-03-15 23:01:28 -0400

    cacher firebase config

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/firestore';
 // Ajouter votre objet de configuration Firebase
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDg41zloqDvnieRUqiCmNui_HiVCSZc0OA",
-  authDomain: "ex3-produits-a2b6c.firebaseapp.com",
-  projectId: "ex3-produits-a2b6c",
-  storageBucket: "ex3-produits-a2b6c.appspot.com",
-  messagingSenderId: "941720093189",
-  appId: "1:941720093189:web:a8dfcdc22b5b12c713926c",
-  measurementId: "G-4PZN5CQ69X"
+// Privé
 };
 
 // Initialiser Firebase
